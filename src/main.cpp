018d58ef9be8f5043fff5044d471f8ce48b5ea09
Thomas Marchand <thomas.marchand@tuta.io>
2020-11-15 11:36:19 +0100

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <ncurses.h>
+
+int main(void)
+{	
+    initscr();              // Initialise la structure WINDOW et autres paramètres 
+    printw("Hello World");  // Écrit Hello World à l'endroit où le curseur logique est positionné
+    refresh();              // Rafraîchit la fenêtre courante afin de voir le message apparaître
+    getch();                // On attend que l'utilisateur appui sur une touche pour quitter
+    endwin();               // Restaure les paramètres par défaut du terminal
+    
+    return 0;
+}
