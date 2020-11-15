c986fbfd9c956f89ba458ca2ae525543925a87c7
Thomas Marchand <thomas.marchand@tuta.io>
2020-11-15 15:15:15 +0100

    instal CMake

--- shell.nix
+++ shell.nix
@@ -1,5 +1,5 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
     name = "poyuterm-dev-environment";
-    buildInputs = [ pkg-config ncurses ];
+    buildInputs = [ pkg-config ncurses cmake ];
 }
