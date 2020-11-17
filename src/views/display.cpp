eb0a66e04af6623a8305124f1c27933ddd973df7
Thomas Marchand <thomas.marchand@tuta.io>
2020-11-17 13:55:16 +0100

    improve display

--- src/views/display.cpp
+++ src/views/display.cpp
@@ -1,23 +1,42 @@
 
 #include <ncurses.h>
 #include "views/display.hpp"
+#include <string>
 
 ConsoleDisplay::ConsoleDisplay(InputsListener &inputsListenerReference)
 {
     inputsListener = inputsListenerReference;
 }
 
+void ConsoleDisplay::setCell(int x, int y, Grid::PuyoType puyo)
+{
+    move(x * virtualScale, y * virtualScale);
+    addch('#');
+}
+
 void ConsoleDisplay::start()
 {
     initscr();
+    int boxHeight = LINES - 2;
+    virtualScale = (boxHeight - boxHeight % 12) / 12; // it must be superior to 0
+
     intrflush(stdscr, FALSE);
     keypad(stdscr, TRUE);
-    int inputCode;
-    do
+    if (COLS > 20 + 2 + 12 && virtualScale > 0)
     {
-        inputCode = getch();
-        inputsListener.onKeyPressed(inputCode);
-    } while (inputCode != KEY_SDL);
+        int inputCode;
+        do
+        {
+            inputCode = getch();
+            inputsListener.onKeyPressed(inputCode);
+        } while (inputCode != KEY_SDL);
+        endwin();
+    }
+    else
+    {
+        printw("Error: your terminal is too small \nminimum cols: 44 \nminimum lines: 14");
 
-    endwin();
+        getch();
+        endwin();
+    }
 }
--- src/views/display.hpp
+++ src/views/display.hpp
@@ -1,12 +1,16 @@
 #pragma once
 
 #include "controllers/gameManager.hpp"
+#include "domains/grid.hpp"
 
 class ConsoleDisplay
 {
 public:
     ConsoleDisplay(InputsListener &inputsListener);
+    void setCell(int x, int y, Grid::PuyoType puyo);
     void start();
+
 private:
+    int virtualScale;
     InputsListener inputsListener;
 };
