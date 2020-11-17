7f3ca27375139151a1baaae67eabff05a346c769
Thomas Marchand <thomas.marchand@tuta.io>
2020-11-17 10:51:45 +0100

    introduce InputsListener

--- src/controllers/gameManager.cpp
+++ src/controllers/gameManager.cpp
@@ -11,8 +11,31 @@ void loop()
     GameData gameData = GameData();
     Grid grid = Grid(content);
 
+    // load inputs controller
+    InputsListener inputsListener = InputsListener();
+
     // load views
     ConsoleDisplay display = ConsoleDisplay();
 
     display.start();
+}
+
+InputsListener::InputsListener() {
+
+}
+
+void InputsListener::onKeyPressed(int code) {
+
+}
+
+void InputsListener::onRightMove() {
+
+}
+
+void InputsListener::onLeftMove() {
+
+}
+
+void InputsListener::onDownMove() {
+
 }
--- src/controllers/gameManager.hpp
+++ src/controllers/gameManager.hpp
@@ -1,3 +1,15 @@
 #pragma once
 
 void loop();
+
+class InputsListener
+{
+public:
+    InputsListener();
+
+private:
+    void onKeyPressed(int code);
+    void onRightMove();
+    void onLeftMove();
+    void onDownMove();
+};
