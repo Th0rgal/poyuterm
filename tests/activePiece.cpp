a25ca5101067c68dd173318c95f8c239fa686e77
Thomas Marchand <thomas.marchand@tuta.io>
2020-12-02 16:31:24 +0100

    add tests for rotation

--- tests/activePiece.cpp
+++ tests/activePiece.cpp
@@ -31,14 +31,45 @@ TEST_CASE("testing rotate")
 {
     std::vector<std::vector<Grid::PuyoType>> content(6, std::vector<Grid::PuyoType>(12));
 
-    ActivePiece activePiece(
+    ActivePiece activePiece{
         Puyo(Grid::red, 2, 0),
-        Puyo(Grid::green, 1, 0), 0);
+        Puyo(Grid::green, 1, 0), 0};
 
     Grid grid = Grid(content);
-    bool moved = activePiece.rotate(grid);
 
+    bool moved = activePiece.rotate(grid);
     CHECK(moved == true);
     CHECK(activePiece.center == Puyo(Grid::red, 1, 0));
     CHECK(activePiece.side == Puyo(Grid::green, 1, 1));
+
+    moved = activePiece.rotate(grid);
+    CHECK(moved == true);
+    CHECK(activePiece.center == Puyo(Grid::red, 1, 0));
+    CHECK(activePiece.side == Puyo(Grid::green, 2, 0));
+
+    moved = activePiece.rotate(grid);
+    CHECK(moved == true);
+    CHECK(activePiece.center == Puyo(Grid::red, 1, 1));
+    CHECK(activePiece.side == Puyo(Grid::green, 1, 0));
+
+    moved = activePiece.rotate(grid);
+    CHECK(moved == true);
+    CHECK(activePiece.center == Puyo(Grid::red, 2, 0));
+    CHECK(activePiece.side == Puyo(Grid::green, 1, 0));
+
+    moved = activePiece.rotate(grid, 4);
+    CHECK(moved == true);
+    CHECK(activePiece.center == Puyo(Grid::red, 2, 0));
+    CHECK(activePiece.side == Puyo(Grid::green, 1, 0));
+
+    moved = activePiece.rotate(grid, 3);
+    CHECK(moved == true);
+    CHECK(activePiece.center == Puyo(Grid::red, 1, 1));
+    CHECK(activePiece.side == Puyo(Grid::green, 1, 0));
+
+    activePiece = ActivePiece{
+        Puyo(Grid::red, 5, 0),
+        Puyo(Grid::green, 5, 1), 1};
+    moved = activePiece.rotate(grid, 1);
+    CHECK(moved == false);
 }
