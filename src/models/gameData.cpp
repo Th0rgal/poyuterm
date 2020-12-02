8b530e812923dc32d8ad698e67300c649c7094d7
Thomas Marchand <thomas.marchand@tuta.io>
2020-12-02 18:00:48 +0100

    test gameData

--- src/models/gameData.cpp
+++ src/models/gameData.cpp
@@ -4,15 +4,27 @@
 #include <math.h>
 
 /**
- * to intialize the GameData
+ * to initialize the GameData
  * 
  * @author Thomas Marchand
+ * @confidence 5
  **/
 GameData::GameData()
 {
     loaded = true;
 }
 
+/**
+ * to calculate and add the score
+ * @param std::size_t groupSize the amount of Puyos in the detected group
+ * @param unsigned int combosIndex the index of the combos (starting à 1)
+ * @param unsigned int groupsNumber the amount of detected groups
+ * 
+ * @return unsigned int the sum of the current score and the newly calculated 
+ * score bonus with a limit of 999999
+ * 
+ * @confidence 5
+ **/
 unsigned int GameData::addScore(std::size_t groupSize, unsigned int combosIndex, unsigned int groupsNumber)
 {
     score = score + 10 * groupSize * (pow(4, combosIndex) + pow(3, groupsNumber));
